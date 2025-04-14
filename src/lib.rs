b0c4cce93484222d37aed79f7865ad5e5e9dd8fd
Borber <borber.drum@gmail.com>
2025-04-14 17:04:44 +0800

    feat: add ssh example

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod common;
 pub mod dns;
 pub mod error;
 pub mod forward;
-mod localhost;
+pub mod localhost;
 pub mod parse;
 pub mod reverse;
 
@@ -15,7 +15,7 @@ use async_trait::async_trait;
 pub use gm_quic::EndpointAddr;
 
 #[async_trait]
-trait Resolver {
+pub trait Resolver {
     async fn publish(&self, name: &str, addresses: &[EndpointAddr]) -> io::Result<()>;
     async fn look_up(&self, name: &str) -> io::Result<Vec<EndpointAddr>>;
 }
